94d2681fe3e5ac589646ae3460b9928618a0c013
Nicola Milne <nicolalouisemilne@gmail.com>
2022-05-20 18:23:40 -0600

    commit tree ed05acc8dbe927dfb1464ec1b5e3c4da017e168e

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Some questions to consider:
+
+// How can we "iterate" through individual words in a string?
+// Can we execute an iteration inside an iteration? How?
+// How can we capitalize just the first letter in a word?
+
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -11,6 +18,34 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+//function sentenceSplitter(sentences){
+//     let splitSentence = sentences[0].split(" ")
+//    
+// }
+// sentenceSplitter()
+
+// const phrase = 'what does the this keyword mean?'
+
+
+// function singleSplitter(arr){
+//   //Take phrase and create a new array of words split at the empty space.
+//   let words = arr.split(" ")
+//   console.log(words);
+//   let wordsCased = words.forEach((word) => {
+//     console.log(word.charAt(0).toUpperCase() + word.slice(1));
+//   });
+//   //use forEach to call .toUpperCase() on the first letter of each word in the splitSentence array and return the complete word
+
+//   // return .join() to bring all the words from the splitSentence array back into a single array with each word capitalized
+// }
+
+function titleCased() {
+  return tutorials.map((tutorial) => 
+    tutorial.split(' ').map((word) => 
+      word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' '));
 }
+
+// capitalWords(tutorials)
+
+// singleSplitter(phrase);
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,18 @@
 describe('index.js', function () {
   describe('titleCased()', function () {
     it('returns an array with title case tutorial names', function () {
-      expect(titleCased()).to.have.all.members(
+      expect(titleCased([
+        'what does the this keyword mean?',
+        'What is the Constructor OO pattern?',
+        'implementing Blockchain Web API',
+        'The Test Driven Development Workflow',
+        'What is NaN and how Can we Check for it',
+        'What is the difference between stopPropagation and preventDefault?',
+        'Immutable State and Pure Functions',
+        'what is the difference between == and ===?',
+        'what is the difference between event capturing and bubbling?',
+        'what is JSONP?'
+      ])).to.have.all.members(
         [
           "What Does The This Keyword Mean?",
           "What Is The Constructor OO Pattern?",
